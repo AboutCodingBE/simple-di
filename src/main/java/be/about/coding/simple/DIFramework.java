9ed4795a6b68eff8db0cf8b3452c4a2afe893f39
Nicholas Ocket <nicholas@aboutcoding.be>
2024-03-26 21:48:26 +0100

    some more comments

--- src/main/java/be/about/coding/simple/DIFramework.java
+++ src/main/java/be/about/coding/simple/DIFramework.java
@@ -1,5 +1,8 @@
 package be.about.coding.simple;
 
+/**
+ * Has the responsibility to build all the dependencies in the right order and to inject them in the right order.
+ */
 public class DIFramework {
 
     public static Service createService() {
