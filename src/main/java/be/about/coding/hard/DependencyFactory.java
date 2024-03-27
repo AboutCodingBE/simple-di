08d89100f3f6ed87d624c73201a3d2ca6678a5d2
Nicholas Ocket <nicholas@aboutcoding.be>
2024-03-27 21:19:30 +0100

    an even better example

--- src/main/java/be/about/coding/hard/DependencyFactory.java
+++ src/main/java/be/about/coding/hard/DependencyFactory.java
@@ -6,9 +6,9 @@ public class DependencyFactory {
         // it is ok to know where to get the properties for this connection as it is my responsibility to create
         // all of these objects
 
-        var url = "the-url-from-configuration";
-        var username = "the-username-from-configuration";
-        var password = "the-password-from-configuration";
+        var url = "jdbc:postgress://localhost/database";
+        var username = "admin";
+        var password = "admin";
 
         return new DatabaseConnection(url, username, password);
     }
